e4769a31f5a170f30ae3281234f73beb2dbae2b5
Nurlan Umarov <you@example.com>
2019-02-13 01:52:54 +0400

    hrsite

--- hrsystem/settings.py
+++ hrsystem/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'hrsystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hrsite_db',
-        'USER': 'hrsite_db_user',
-        'PASSWORD': '!!hrsite!!05',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'hrsite_db',
+#         'USER': 'hrsite_db_user',
+#         'PASSWORD': '!!hrsite!!05',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
