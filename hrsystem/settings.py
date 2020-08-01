fefc01e325ddf3473e46ca31130a0c617254afa2
Your Name <you@example.com>
2020-08-01 10:35:03 +0400

    data

--- hrsystem/settings.py
+++ hrsystem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')%+!g&jjj!41j7v*v-kbjs(#)3gada(8j26n5csbre$c-nm59t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
